c663a0b4185409ccb3d2c8a7b39b735ad28be530
SwayamChandak <chandakswayam3904@gmail.com>
2025-06-24 14:31:23 +0530

    removed pgsql credentials

--- db.py
+++ db.py
@@ -13,7 +13,7 @@ DBSession: sessionmaker = None
 def init_db():
     try:
         global DBSession, engine
-        pg_url='postgresql://postgres:Password.postgresql3904@localhost:5432/db'
+        pg_url='postgresql://{username}:{password}@localhost:5432/{db_name}'
         engine=create_engine(pg_url)
         conn=engine.connect()
         conn.close()
